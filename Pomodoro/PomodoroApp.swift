c60ca586ce2bb84b9c49c1e91ca3e9fb37d1b65c
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2022-08-29 12:39:52 -0400

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Ethan Kisiel on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
