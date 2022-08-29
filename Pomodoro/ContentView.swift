754299a47cf89a37b6ca00b2d71a799091e0b067
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2022-08-29 13:02:16 -0400

    Added audio files and added Sound Manager functionality

--- Pomodoro/ContentView.swift
+++ Pomodoro/ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world!").onTapGesture {
+                SoundManager.shared.playSound(soundName: "button-press")
+            }
         }
     }
 }
--- Pomodoro/Managers/SoundManager.swift
+++ Pomodoro/Managers/SoundManager.swift
@@ -11,5 +11,18 @@ import Foundation
 class SoundManager
 {
     static let shared = SoundManager()
+    var player: AVAudioPlayer?
     
+    func playSound(soundName: String)
+    {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "mp3") else { return }
+        do
+        {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error
+        {
+            print("Error initializing audio player. \(error.localizedDescription)")
+        }
+    }
 }
